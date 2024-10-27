3bc964ab786755b3ad5e11a8e20af28d794038c7
buddy <buddyglobalco@gmail.com>
2024-10-26 21:42:57 -0400

    commit made to product page

--- productpagescript.js
+++ productpagescript.js
@@ -24,3 +24,23 @@ function forwardslide(n){
     slideproductimage(slideindex += n )
 }
 console.log(slideproductimage)
+
+var advertindex = 0;
+
+function adslide(){
+
+    var slideshowad = document.querySelectorAll(".slideshowad");
+    advertindex++;
+    if(advertindex > slideshowad.length){
+        advertindex = 1
+    }
+    var i
+    for(i = 0;i < slideshowad.length;i++){
+        slideshowad[i].style.display = "none "
+    }
+slideshowad[advertindex-1].style.display = " block"
+   setTimeout(adslide,2000) 
+ 
+}
+adslide()
+console.log(adslide)
